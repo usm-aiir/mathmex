c9a55fa7b1913c34f625962fa57de6e3aba80ae8
Clayton Durepos <145684590+CDurepos@users.noreply.github.com>
2025-07-12 15:26:55 -0400

    Added indexing scripts

---
+++ backend/admin/delete_index_data.py
@@ -0,0 +1,30 @@
+from opensearchpy import OpenSearch
+
+# --- CONFIG ---
+HOST = 'localhost'
+PORT = 9200
+USER = 'admin'
+PASSWORD = 'Str0ngP0ssw0rd'
+INDEX_NAME = 'mathmex'
+
+# --- Connect ---
+client = OpenSearch(
+    hosts=[{'host': HOST, 'port': PORT}],
+    http_auth=(USER, PASSWORD),
+    use_ssl=True,
+    verify_certs=False,
+    ssl_show_warn=False
+)
+
+# --- Delete all documents ---
+response = client.delete_by_query(
+    index=INDEX_NAME,
+    body={
+        "query": {
+            "match_all": {}
+        }
+    }
+)
+
+print(f"Deleted documents from index '{INDEX_NAME}':")
+print(response)
---
+++ backend/admin/mappings.py
@@ -0,0 +1,21 @@
+mapping = {
+    "properties": {
+        "title": {"type": "text"},
+        "media_type": {"type": "text"},
+        "body_text": {"type": "text", "fielddata": True},
+        "body_vector": {
+            "type": "knn_vector",
+            "dimension": 768,
+            "method": {
+                "name": "hnsw",
+                "space_type": "cosinesimil",
+                "engine": "nmslib",
+                "parameters": {
+                    "ef_construction": 128,
+                    "m": 16
+                }
+            }
+        },
+        "link": {"type": "keyword"}
+    }
+}
